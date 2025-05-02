be52b080e12583d8f4d0830e86e185a7a4006aa0
Lawrence Valerio <valerio.lawrence@gmail.com>
2025-05-01 20:58:56 -0500

    Made a change to the login layout to not have duplicate AuthProvider setup

---
+++ src/app/auth/login/layout.tsx
@@ -0,0 +1,13 @@
+import "./globals.css";
+
+export default function LoginLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
