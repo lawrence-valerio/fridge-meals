732ed1b96c4a68864683ca3a6fd4e71fe3a7c249
Lawrence Valerio <valerio.lawrence@gmail.com>
2025-05-01 20:13:12 -0500

    Added Login button for login form

--- src/app/login/components/LoginForm.tsx
+++ src/app/login/components/LoginForm.tsx
@@ -1,3 +1,4 @@
+import { ChevronRightIcon } from "lucide-react";
 import { useState } from "react";
 
 interface LoginFormProps {
@@ -37,11 +38,19 @@ export default function LoginForm({ onSubmit }: LoginFormProps) {
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          className=""
+          className="mt-1 w-fill px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500"
         />
       </div>
 
-      <button type="submit">Login</button>
+      <button
+        type="submit"
+        className="w-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white py-2 px-4 rounded-lg font-medium hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 flex items-center justify-center gap-2"
+      >
+        <>
+          Get Started
+          <ChevronRightIcon size={18} />
+        </>
+      </button>
     </form>
   );
 }
