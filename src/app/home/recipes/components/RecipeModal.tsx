5f5e8714d09a1e4fc517729c3e876e658c820a98
Lawrence Valerio <valerio.lawrence@gmail.com>
2025-06-24 16:26:03 -0500

    fixed background transparency when opening the modal

--- src/app/home/recipes/components/RecipeModal.tsx
+++ src/app/home/recipes/components/RecipeModal.tsx
@@ -1,13 +1,31 @@
-export const RecipeModal = () => {
-  return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black bg-opacity-50">
-      <div className="bg-white rounded-lg shadow=xl overflow-hidden w-full max-w-4xl max-h-[90vh] flex flex-col">
+import type { MealSuggestionType } from "./MealSuggestion.types";
+import { XIcon } from "lucide-react";
+import { createPortal } from "react-dom";
+
+export const RecipeModal = ({
+  meal,
+  onClose,
+}: {
+  meal: MealSuggestionType;
+  onClose: () => void;
+}) => {
+  if (typeof window === "undefined") return null;
+  return createPortal(
+    <div className="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black/50">
+      <div className="bg-white rounded-lg shadow-xl overflow-hidden w-full max-w-4xl max-h-[90vh] flex flex-col">
         <div className="relative">
           <div className="h-64 overflow-hidden">
             <h1>test modal</h1>
           </div>
+          <button
+            onClick={onClose}
+            className="absolute top-4 right-4 bg-white rounded-full p-1 shadow-md hover:bg-gray-100"
+          >
+            <XIcon size={24} className="text-gray-700" />
+          </button>
         </div>
       </div>
-    </div>
+    </div>,
+    document.body
   );
 };
